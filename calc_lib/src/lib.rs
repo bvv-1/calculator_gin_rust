f7eef50703e0533422ec5cc90c8b74b1cea3abca
bvv-1 <fhinnoigsr@gmail.com>
2024-05-19 22:08:55 +0900

    Add tests and functions for basic arithmetic operations

--- calc_lib/src/lib.rs
+++ calc_lib/src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_subtract() {
+        assert_eq!(subtract(2, 1), 1);
+    }
+
+    #[test]
+    fn test_multiply() {
+        assert_eq!(multiply(2, 3), 6);
+    }
+
+    #[test]
+    fn test_divide() {
+        assert_eq!(divide(6, 3), 2);
     }
 }
+
+#[no_mangle]
+pub extern "C" fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[no_mangle]
+pub extern "C" fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+#[no_mangle]
+pub extern "C" fn multiply(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+#[no_mangle]
+pub extern "C" fn divide(a: i32, b: i32) -> i32 {
+    a / b
+}
+
+// #[no_mangle]
+// https://rs.nkmk.me/rust-division-by-zero/
+// pub extern "C" fn divide(a: i32, b: i32) -> Result<i32, &'static str> {
+//     if b == 0 {
+//         Err("Cannot divide by zero")
+//     } else {
+//         Ok(a / b)
+//     }
+// }
